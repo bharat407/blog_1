34a03a0ef34b2ebaca29d4a5415db78e544cfeb9
Bharat Kushwah <bharatkushwah407@gmail.com>
2023-06-10 21:03:40 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import Header from "./components/Header";
 import Blogs from "./components/Blogs";
 import Pagination from "./components/Pagination";
